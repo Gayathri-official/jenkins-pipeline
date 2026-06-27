74ad39820d4578080bccce68e687344cb871eef5
Gayathri-official <mudraboyinagayathri34@gmail.com>
2026-06-27 10:36:07 +0530

    Update App.test.tsx

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,21 +1,8 @@
-<<<<<<< HEAD
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
-
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
-=======
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
->>>>>>> 70579feeae459c314a95321de94e87ad07788bc0
