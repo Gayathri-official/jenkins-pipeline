ffab6fcd9a4db8ee2eae37ebb42a5b7aa58b218c
Gayathri-official <mudraboyinagayathri34@gmail.com>
2026-06-27 10:41:28 +0530

    Update index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
@@ -10,16 +9,3 @@ ReactDOM.render(
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
-=======
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
-registerServiceWorker();
->>>>>>> 70579feeae459c314a95321de94e87ad07788bc0
