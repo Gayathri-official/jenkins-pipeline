b3d1f9156e47e114273110d5867e1bcb36ecdeb1
Gayathri-official <mudraboyinagayathri34@gmail.com>
2026-06-27 10:37:45 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 import * as React from 'react';
 import './App.css';
 import BeerList from './BeerList';
-
 import logo from './logo.svg';
-
 class App extends React.Component<{}, any> {
   render() {
     return (
@@ -18,27 +15,4 @@ class App extends React.Component<{}, any> {
     );
   }
 }
-
-=======
-import * as React from 'react';
-import './App.css';
-import BeerList from './BeerList';
-
-import logo from './logo.svg';
-
-class App extends React.Component<{}, any> {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <BeerList/>
-      </div>
-    );
-  }
-}
-
->>>>>>> 70579feeae459c314a95321de94e87ad07788bc0
 export default App;
