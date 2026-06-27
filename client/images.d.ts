76b7d1c8c11faf50b85f7b1f68e1018e7650dfd7
Gayathri-official <mudraboyinagayathri34@gmail.com>
2026-06-27 10:11:49 +0530

    Update images.d.ts

--- client/images.d.ts
+++ client/images.d.ts
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 declare module '*.svg'
 declare module '*.png'
 declare module '*.jpg'
-=======
-declare module '*.svg'
-declare module '*.png'
-declare module '*.jpg'
->>>>>>> 70579feeae459c314a95321de94e87ad07788bc0
