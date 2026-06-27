bdd573ce2af8af88889ba188360316bf272cfd8c
Gayathri-official <mudraboyinagayathri34@gmail.com>
2026-06-27 10:40:17 +0530

    Update GiphyImage.tsx

--- client/src/GiphyImage.tsx
+++ client/src/GiphyImage.tsx
@@ -1,28 +1,21 @@
-<<<<<<< HEAD
 import * as React from 'react';
-
 interface GiphyImageProps {
   name: string;
 }
-
 interface GiphyImageState {
   giphyUrl: string;
   isLoading: boolean;
 }
-
 class GiphyImage extends React.Component<GiphyImageProps, GiphyImageState> {
   constructor(props: GiphyImageProps) {
     super(props);
-
     this.state = {
       giphyUrl: '',
       isLoading: false
     };
   }
-
   componentDidMount() {
     const giphyApi = '//api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=1&q=';
-
     fetch(giphyApi + this.props.name)
       .then(response => response.json())
       .then(response => {
@@ -35,70 +28,14 @@ class GiphyImage extends React.Component<GiphyImageProps, GiphyImageState> {
         this.setState({isLoading: false});
       });
   }
-
   render() {
     const {giphyUrl, isLoading} = this.state;
-
     if (isLoading) {
       return <p>Loading image...</p>;
     }
-
     return (
       <img src={giphyUrl} alt={this.props.name} width="200"/>
     );
   }
 }
-
-=======
-import * as React from 'react';
-
-interface GiphyImageProps {
-  name: string;
-}
-
-interface GiphyImageState {
-  giphyUrl: string;
-  isLoading: boolean;
-}
-
-class GiphyImage extends React.Component<GiphyImageProps, GiphyImageState> {
-  constructor(props: GiphyImageProps) {
-    super(props);
-
-    this.state = {
-      giphyUrl: '',
-      isLoading: false
-    };
-  }
-
-  componentDidMount() {
-    const giphyApi = '//api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=1&q=';
-
-    fetch(giphyApi + this.props.name)
-      .then(response => response.json())
-      .then(response => {
-        if (response.data.length > 0) {
-          this.setState({giphyUrl: response.data[0].images.original.url});
-        } else {
-          // dancing cat for no images found
-          this.setState({giphyUrl: '//media.giphy.com/media/YaOxRsmrv9IeA/giphy.gif'});
-        }
-        this.setState({isLoading: false});
-      });
-  }
-
-  render() {
-    const {giphyUrl, isLoading} = this.state;
-
-    if (isLoading) {
-      return <p>Loading image...</p>;
-    }
-
-    return (
-      <img src={giphyUrl} alt={this.props.name} width="200"/>
-    );
-  }
-}
-
->>>>>>> 70579feeae459c314a95321de94e87ad07788bc0
 export default GiphyImage;
